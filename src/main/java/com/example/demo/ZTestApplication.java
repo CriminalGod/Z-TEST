77c9354b460bf24c3f36ccad116ada0672be0884
CriminalGod <rakeshkumar1146@yahoo.com>
2021-09-24 06:14:16 +0530

    First Commit through MyEclipse

--- src/main/java/com/example/demo/ZTestApplication.java
+++ src/main/java/com/example/demo/ZTestApplication.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.service.PersonService;
+
 @SpringBootApplication
 public class ZTestApplication {
+	
+	@Autowired
+	PersonService personService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZTestApplication.class, args);
