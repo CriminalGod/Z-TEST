4246a9289763c99c0809b68cb5078bde66659220
CriminalGod <rakeshkumar1146@yahoo.com>
2021-09-24 17:24:36 +0530

    all records deletion and multiple records insertion

--- src/main/java/com/example/demo/ZTestApplication.java
+++ src/main/java/com/example/demo/ZTestApplication.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,22 +18,34 @@ public class ZTestApplication implements CommandLineRunner {
     @Autowired
     private PersonService personService;
 
+    List<Person> persons = Arrays.asList(new Person("variable", "kumar", "variablekumar@gmail.com", new Date()),
+		new Person("property", "kumar", "propertykumar@gmail.com", new Date()),
+		new Person("argument", "kumar", "argumentkumar@gmail.com", new Date()));
+    
     public static void main(String[] args) {
 	SpringApplication.run(ZTestApplication.class, args);
     }
 
     private void createPerson() {
-
-	Person person = new Person("ram", "kumar", "ramkumar@gmail.com", new Date());
-	System.out.println(person);
+	Person person = new Person("kiran", "kumar", "kirankumar@gmail.com", new Date());
 	Person p = personService.createPerson(person);
 	System.out.println(p);
-
     }
 
     @Override
     public void run(String... args) throws Exception {
-	createPerson();
+	clearData();
+//	createPerson();
+	createPersons();
     }
 
+    private void clearData() {
+	personService.clearData();
+    }
+
+    private void createPersons() {
+	Iterable<Person> list = personService.createPersons(persons);
+	list.forEach(System.out::println);
+    }
+    
 }
--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,13 @@ public class PersonService {
 
     public Person createPerson(Person person) {
 	return personRepo.save(person);
+    }
+
+    public Iterable<Person> createPersons(List<Person> persons) {
+	return personRepo.saveAll(persons);
+    }
+
+    public void clearData() {
+	personRepo.deleteAll();
     }
 }
